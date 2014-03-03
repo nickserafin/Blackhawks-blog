1044aac98fe60ba50e86db1ca2608e3b5d97eed6
Nick Serafin <ficknation@gmail.com>
2014-03-02 23:57:49 -0600

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+Hossa Back = Article.create! name: "Hossa Back", content: <<-ARTICLE
+The man, the myth, the legend, big hoss is back fro mteh olympics.  He was seen at practice today along with six other olympians.  THe rest of the team is set to be back tomorrow and will meet up wit hthe team as the get o ntheir way to New York to play the rangers.  This first game back will likely set the tone for the last bit of the regular season.  Lets get a W here boys and take the momentum all the way to the final.
+ARTICLE
+
+superman = Article.create! name: "Superman", content: <<-ARTICLE
+Superman is a fictional comic book superhero appearing in publications by DC Comics, widely considered to be an American cultural icon. Created by American writer Jerry Siegel and Canadian-born American artist Joe Shuster in 1932 while both were living in Cleveland, Ohio, and sold to Detective Comics, Inc. (later DC Comics) in 1938, the character first appeared in Action Comics #1 (June 1938) and subsequently appeared in various radio serials, television programs, films, newspaper strips, and video games. (from Wikipedia)
+ARTICLE
+
+krypton = Article.create! name: "Krypton", content: <<-ARTICLE
+Krypton is a fictional planet in the DC Comics universe, and the native world of the super-heroes Superman and, in some tellings, Supergirl and Krypto the Superdog. Krypton has been portrayed consistently as having been destroyed just after Superman's flight from the planet, with exact details of its destruction varying by time period, writers and franchise. Kryptonians were the dominant people of Krypton. (from Wikipedia)
+ARTICLE
+
+lex_luthor = Article.create! name: "Lex Luthor", content: <<-ARTICLE
+Lex Luthor is a fictional character, a supervillain who appears in comic books published by DC Comics. He is the archenemy of Superman, and is also a major adversary of Batman and other superheroes in the DC Universe. Created by Jerry Siegel and Joe Shuster, he first appeared in Action Comics #23 (April 1940). Luthor is described as "a power-mad, evil scientist" of high intelligence and incredible technological prowess. (from Wikipedia)
+ARTICLE
+
+robin = Article.create! name: "Robin", content: <<-ARTICLE
+Robin is the name of several fictional characters appearing in comic books published by DC Comics, originally created by Bob Kane, Bill Finger and Jerry Robinson, as a junior counterpart to DC Comics superhero Batman. The team of Batman and Robin is commonly referred to as the Dynamic Duo or the Caped Crusaders. (from Wikipedia)
+ARTICLE
+
+
